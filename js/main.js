00f0df72bee96a2ad2770c7561aed47efbc2293e
GabriellaSundstrom <suga24hy@student.ju.se>
2025-04-18 17:06:25 +0200

    displaying in calendar

--- js/main.js
+++ js/main.js
@@ -29,5 +29,54 @@ function addToCalendar(recipeId) {
   }
 }
 
+//Adding to calendar with buttons:
+let addToCalendarButtons = document.querySelectorAll(".add-calendar-button");
+//Once DOM loaded
+document.addEventListener("DOMContentLoaded", function () {
+  let addCalendarButtons = document.querySelectorAll(".add-calendar-button");
+
+  addCalendarButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      alert("Added to calendar!");
+      let recipeId = button.getAttribute("data-recipe-id");
+      addToCalendar(recipeId);
+    });
+  });
+});
+
 //localStorage.clear();
 //addToCalendar();
+
+//Displaying recipes in calendar:
+document.addEventListener("DOMContentLoaded", function () {
+  let calendarContainer = document.getElementById("calendar-container");
+
+  if (calendarContainer) {
+    //display the recipe image
+    let calendar = JSON.parse(localStorage.getItem("calendar")) || []; // Get calendar from localStorage
+    if (calendar.length > 0) {
+      // Loop through the cart and display each product
+      calendar.forEach(function (recipe) {
+        let recipeDiv = document.createElement("div");
+        recipeDiv.classList.add("calendar-item");
+
+        let recipeHTML = `
+        <div class="added-recipes">
+                <div class="calendar-recipe-image">
+                    <img src="${recipe.image}" alt="${recipe.name}" />
+                </div>
+                    <h3 class="recipe-name">${recipe.name}</h3>
+                    </div>
+                `;
+
+        recipeDiv.innerHTML = recipeHTML;
+
+        // Append the recipe to the calendar container
+        calendarContainer.appendChild(recipeDiv);
+      });
+    } else {
+      // If calendar empty:
+      calendarContainer.innerHTML = "<p>Add recipes you want to make!</p>";
+    }
+  }
+});
